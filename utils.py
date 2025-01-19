99b5f81e6bffeff98817ffc0a79df95ea05b9201
DP <pham.huuduc611@gmail.com>
2025-01-19 09:05:32 +0000

    moved SST2Dataset to utils

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Dataset
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def get_loss_and_accuracy(model, dataloader, device, eval_ratio=1.0):
     final_accuracy = 0
@@ -32,3 +33,21 @@ def get_loss_and_accuracy(model, dataloader, device, eval_ratio=1.0):
     final_accuracy /= len(dataloader)
     final_loss /= len(dataloader)
     return loss, final_accuracy
+
+class SST2Dataset(Dataset):
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+    
+    def __len__(self):
+        return len(self.y)
+
+    def __getitem__(self, idx):
+        x = {
+            'input_ids': self.X['input_ids'][idx, :].to(device),
+            'attention_mask': self.X['attention_mask'][idx, :].to(device)
+        }
+        label = self.y[idx]
+        y = torch.Tensor([1.0, 0.0]) if label == 0 else torch.Tensor([0.0, 1.0])
+        y = y.to(device)
+        return x, y
